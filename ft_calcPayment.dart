26516e9214f24ad027076393279b9caf7dba9b84
artimmy <arthurtimmy@hotmail.com>
2022-01-11 17:43:55 -0300

    using '=>' to write a simple func

--- ft_calcPayment.dart
+++ ft_calcPayment.dart
@@ -1,14 +1,14 @@
 void main()
 {
   double bonus = 500;
-  double result = calcPayment(1000);
+  double result = ft_calcPayment(1000);
   double total = result + bonus;
   print("Total payment: $total");
 
 }
 
 
-/* double calcPayment(double payment)
+/* double ft_calcPayment(double payment)
 {
   double total = payment - (payment * 0.1);
   return total;
@@ -16,4 +16,4 @@ void main()
 }
  */
 //                                \/ this '=>' is equivalent to return the result of the following, but, if the function has more than 2 or 3 line the recommended would be to write it normally 
-double calcPayment(double payment) => payment - (payment * 0.1);
+double ft_calcPayment(double payment) => payment - (payment * 0.1);
