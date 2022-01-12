825c22a0870626563cbd74f9ece942c0fb6273e9
artimmy <arthurtimmy@hotmail.com>
2022-01-12 20:10:26 -0300

    updated constructor_override.dart

--- constructor_override.dart
+++ constructor_override.dart
@@ -1,10 +1,16 @@
+/*pt-br: esse programa sobrescreve um construtor da classe pai/super 
+1 na main o construtor das classes recebe o valor da "color" e a cor especifica de cada animal e manda pra cada classe respectiva
+2 a classe Dog/Bird recebe a cor geral da classe super e recebe a sua cor especifica
+3 de volta na main o print recebe a propriedade "cor" que é generalizada e depois recebe a cor especifica
+
+*/
 void main()
 {
   Dog dog = new Dog("Black","White");
   Bird bird = new Bird("Blue","Green");
 
   print("The dog's color is ${dog.color} and hes ears are ${dog.cEar}");
-  print("The bird's color is ${bird.color} and hes Fethers are ${bird.cFether}");
+  print("The bird's color is ${bird.color} and hes fethers are ${bird.cFether}");
 }
 
 class Animal
