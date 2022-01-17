e40f309068cc98983101156db275e3ee40112ae0
artimmy <arthurtimmy@hotmail.com>
2022-01-17 15:58:23 -0300

    adding dart00.dart

---
+++ dart00.dart
@@ -0,0 +1,15 @@
+import "dart:io";
+void main()
+{
+  List<String> words = ["A", "B", "C"];
+  smash(words);
+}
+
+String? smash(words) 
+{
+  // your code here
+  for (String word in words) 
+  {
+    stdout.write("${word} ");
+  }
+}
