1a915e86e22e31fd2caa72cc2766ddba2b6fa79a
artimmy <arthurtimmy@hotmail.com>
2022-01-17 17:16:09 -0300

    adding dart01.dart

---
+++ dart01.dart
@@ -0,0 +1,8 @@
+void main()
+{
+  String str = "% e5`_5a\$<<<<<<< {?* \´<<<<<<< ({)d 0_d  [,ea/\$<<<<<<<<< ?%7,{ 3..[&{97@1;&` `f 3 / =e? ";
+  //paste your string on str, remember that 
+  print(noSpace(str));
+}
+
+String noSpace(String x) => x.replaceAll(' ', '');
