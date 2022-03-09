c655acc7cfee9740e3f0fb968405e52adbbde6dc
artimmy <arthurtimmy@hotmail.com>
2022-03-09 02:08:28 -0300

    Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.

---
+++ dart11.dart
@@ -0,0 +1,20 @@
+void main()
+{
+  print(longest("thisisalongstring", "thisisanotherreallylongstring"));
+}
+
+String longest(String a, String b)
+{
+  // Concatenates both strings
+  String c = a + b;
+  // Split each letter and puts them in a List
+  List<String> result = c.split('');
+  // Sorts them alphabetically
+  result.sort();
+  // Remove duplicates
+  result = result.toSet().toList();
+  // List<String> deleteDuplicates = LinkedHashSet<String>.from(concatenate).toList(); // works to >>> import 'dart:collection';
+  // Convert to string
+  String end = result.join();
+  return end;
+}
